df025cdba54ab74999d4ea015289c7e65242ce81
Jeremi Biernacki <jeremi360@gmail.com>
2020-11-30 12:09:40 +0100

    fixes characters and menu in intro.gd

--- game/scenes/intro.gd
+++ game/scenes/intro.gd
@@ -3,14 +3,12 @@ extends Dialogue
 const later = 1
 const now = 2
 
+func _ready() -> void:
+	Rakugo.define_character("Me", "m", Color.skyblue)
+	Rakugo.define_character("Sylive", "s", Color.pink)
+
 func intro_dialog():
 	start_event("intro_dialog")
-
-	# load characters and make them saveable
-	var me = load("res://game/characters/me.tres")
-	Rakugo.StoreManager.set("m", me)
-	var sylive = load("res://game/characters/sylvie/sylvie.tres")
-	Rakugo.StoreManager.set("s", sylive)
 
 	show("bg lecturehall")
 	say(null,
@@ -62,17 +60,16 @@ func intro_dialog():
 	step()
 	
 	var choice = menu([
-		["To ask her right away.", later, {}],
-		["To ask her later.", now, {}],
+		["To ask her right away.", now, {}],
+		["To ask her later.", later, {}],
 	])
-
-	# to make menu use safe for rollback
+	
 	if is_active():
 		if cond(choice == now):
 			$rightaway.start()
 
 		elif cond(choice == later):
 			$later.start()
-
+	
 	exit()
 	end_event()
