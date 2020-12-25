2c275240e6e6ef668c584da63a13e2b5b3512e29
Jeremi <jeremi360@gmail.com>
2020-12-25 09:19:42 +0100

    fix after marge

--- gui/InGame/DialoguePanel/AskEntry.gd
+++ gui/InGame/DialoguePanel/AskEntry.gd
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-extends LineEdit
-
-var variable_name:String = ""
-
-func _ready():
-	Rakugo.connect("ask" ,self, "_on_ask")
-
-
-func _on_ask(default_answer, _parameters):
-	if _parameters.has('placeholder'):
-		self.placeholder_text = _parameters['placeholder']
-	self.text = default_answer
-	show()
-
-
-func _on_text_entered(new_text):
-	hide()
-	Rakugo.ask_return(new_text)
-=======
 extends LineEdit
 
 var variable_name:String = ""
@@ -50,4 +30,3 @@ func _on_text_entered(new_text):
 
 
 
->>>>>>> f4a81ea87c9bba54a08d3ac43a556af80a7ac575
--- gui/InGame/DialoguePanel/DialoguePanel.gd
+++ gui/InGame/DialoguePanel/DialoguePanel.gd
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-extends PanelContainer
-
-
-func _ready():
-	Rakugo.connect("say" ,self, "_on_say")
-	Rakugo.connect("ask" ,self, "_on_ask")
-
-func _on_say(_character, _text, _parameters):
-	show()
-
-func _on_ask(_default_answer, _parameters):
-	show()
-
-func _step():
-	hide()
-=======
 extends PanelContainer
 
 func _ready():
@@ -29,4 +12,3 @@ func _on_ask(_default_answer, _parameters):
 
 func _step():
 	hide()
->>>>>>> f4a81ea87c9bba54a08d3ac43a556af80a7ac575
