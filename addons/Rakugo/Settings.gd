9f207a5237940e8bbb7a972460dd4fdade65c16d
Jeremi Biernacki <jeremi360@gmail.com>
2021-03-24 08:18:41 +0100

    Reformat, default Narrator.name = ""

--- addons/Rakugo/Settings.gd
+++ addons/Rakugo/Settings.gd
@@ -2,17 +2,13 @@ extends Node
 
 const default_window_size := Vector2(1024, 600)
 
-
 var audio_buses := {}
-
 
 signal window_size_changed(prev, now)
 signal window_fullscreen_changed(value)
 
 func _ready() -> void:
 	load_property_list()
-
-
 
 func save_conf() -> void:
 	var config = ConfigFile.new()
@@ -33,18 +29,15 @@ func save_conf() -> void:
 		audio_buses[bus_name] = {"mute":mute, "volume": volume}
 
 
-
 func load_property_list():
 	if Rakugo.persistent.get('settings'):
 		property_list = Rakugo.persistent.get('settings')
 	else:
 		save_property_list()
 
-
 func save_property_list():
 	Rakugo.persistent.settings = property_list
 	Rakugo.StoreManager.save_persistent_store()
-
 
 func get(property, default=null, set_default=true, project_setting_only=false):
 	if not project_setting_only and property in property_list:
@@ -57,7 +50,6 @@ func get(property, default=null, set_default=true, project_setting_only=false):
 		property_list[property] = default
 	return default
 
-
 func set(property, value, save_changes=true):
 	property_list[property] = value
 	if save_changes:
@@ -65,5 +57,4 @@ func set(property, value, save_changes=true):
 
 
 var property_list:Dictionary = {}
-
 var default_property_list:Dictionary = SettingsList.new().default_property_list
