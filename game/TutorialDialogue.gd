dc68ca35134017052915430e52dd53a961986b07
Jeremi Biernacki <jeremi360@gmail.com>
2021-06-27 15:11:38 +0200

    More tutorial base

--- game/TutorialDialogue.gd
+++ game/TutorialDialogue.gd
@@ -20,7 +20,7 @@ func intro():
 			start("script_tut")
 
 		"Markups":
-			pass
+			start("markups")
 
 		"showing":
 			pass
@@ -30,19 +30,81 @@ func intro():
 	
 	end_event()
 
+func markups():
+	start_event("markups")
+
+	say(null, "Choose Scripting Tutorial")
+	var markup = menu([
+		"Markdown Example",
+		"Ren`Py Example",
+		"BBCode Example",
+		"Go Back"
+	])
+
+	match markup:
+		"Markdown Example":
+			show("code")
+			# show("rakugo_text")
+			pass
+
+		"Ren`Py Example":
+			show("code")
+			# show("rakugo_text")
+			pass
+
+		"BBCode Example":
+			show("code")
+			# show("rakugo_text")
+			pass
+
+		"Go Back":
+			start("intro")
+
+
+	end_event()
+
 func script_tut():
 	start_event("script_tut")
 
 	say(null, "Choose Scripting Tutorial")
-	var begin_from = menu([
+	var script = menu([
 		"Creating Dialogue",
 		"Say", "Ask", "Menu",
 		"Notify", "Characters",
-		"Using Global Variables"
+		"Using Global Variables",
+		"Go Back"
 	])
+	
+	match script:
+		"Creating Dialogue":
+			show("code")
+			pass
 
-	show("code")
+		"Say":
+			show("code")
+			pass
 
+		"Ask":
+			show("code")
+			pass
 
+		"Menu":
+			show("code")
+			pass
+
+		"Notify":
+			show("code")
+			pass
+
+		"Characters":
+			show("code")
+			pass
+			
+		"Using Global Variables":
+			show("code")
+			pass
+
+		"Go Back":
+			start("intro")
 
 	end_event()
