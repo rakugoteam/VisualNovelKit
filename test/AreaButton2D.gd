3ef561e58222c0f8bc722a70208cd7adb2328543
Jeremi Biernacki <jeremi360@gmail.com>
2021-06-30 08:53:43 +0200

    add pressed support for AreaButton2D

--- test/AreaButton2D.gd
+++ test/AreaButton2D.gd
@@ -1,24 +1,34 @@
 extends Area2D
 class_name AreaButton2D, "res://addons/Rakugo/icons/button_2d.svg"
 
+export var use_global_theme := false
 export var theme: Resource
-export var hover_color := Color.yellowgreen
-export var highlight_color: Color = modulate
-export var disabled_color := Color.gray 
+export var hover_color := Color.blue
+export var highlight_color := Color.white
+export var pressed_color := Color.darkblue
+export var disabled_color := Color.gray
 export var disabled : bool setget _set_disabled, _get_disabled
+export var pressed : bool setget _set_pressed, _get_pressed
 
+var _pressed := false
 var _disabled := false
 
 var idle_color: Color = modulate
 var is_mouse_in := false
 
-func _ready():
+signal pressed
+
+func _ready() -> void:
+	if use_global_theme:
+		theme = Settings.get(SettingsList.theme)
+
 	if theme and theme is RakugoTheme:
 		theme = theme as RakugoTheme
 		hover_color = theme.hover_node_color
 		highlight_color = theme.focus_node_color
 		idle_color = theme.idle_node_color
 		disabled_color = theme.disable_node_color
+		pressed_color = theme.pressed_node_color
 
 	connect("body_entered", self, "_on_body_entered")
 	connect("body_exited", self, "_on_body_exited")
@@ -33,7 +43,17 @@ func _set_disabled(value:bool) -> void:
 func _get_disabled() -> bool:
 	return _disabled
 
-func _on_body_entered(body):
+func _set_pressed(value:bool) -> void:
+	_pressed = value
+	modulate = idle_color
+
+	if _pressed:
+		modulate = pressed_color
+	
+func _get_pressed() -> bool:
+	return _pressed
+
+func _on_body_entered(body:PhysicsBody2D) -> void:
 	if _disabled:
 		return
 
@@ -42,7 +62,7 @@ func _on_body_entered(body):
 		is_mouse_in = true
 		emit_signal("mouse_entered")
 
-func _on_body_exited(body):
+func _on_body_exited(body:PhysicsBody2D) -> void:
 	if _disabled:
 		return
 
@@ -51,5 +71,16 @@ func _on_body_exited(body):
 		is_mouse_in = false
 		emit_signal("mouse_exited")
 
+func _input(event:InputEvent) -> void:
+	if is_mouse_in:
+		if not pressed:
+			if event is InputEventMouseButton:
+				var button = event as InputEventMouseButton
+				if button.button_index == BUTTON_LEFT:
+					modulate = pressed_color
+					pressed = true
+					emit_signal("pressed")
+					print("pressed")
 
-#todo pressed and highlight support
+
+#todo add highlight support
