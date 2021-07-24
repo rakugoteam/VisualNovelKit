019d23850187256247f57d4e167bd5f916999cbb
Jeremi Biernacki <jeremi360@gmail.com>
2021-07-24 21:25:31 +0200

    start Create Dialogue branch

---
+++ game/ScriptingDialogue.gd
@@ -0,0 +1,65 @@
+extends Dialogue
+
+
+func script_tut():
+	start_event("script_tut")
+
+	say(null, "Choose Scripting Tutorial")
+	var script = menu([
+		"Creating Dialogue",
+		"Say", "Ask", "Menu",
+		"Notify", "Characters",
+		"Using Global Variables",
+		"Go Back"
+	])
+	
+	match script:
+		"Creating Dialogue":
+			say(null, "First, the crate Dialogue Node")
+			show("screenshot create_dialogue")
+			step()
+
+			say(null, "Change it name")
+			# show(screen_shot_of_this)
+			step()
+
+			say(null, "Choose Extend script")
+			# show(screen_shot_of_this)
+			step()
+
+			say(null, "Now you can add code like this to crate dialogue")
+			show("code dialogue")
+			pass
+
+		"Say":
+			show("code")
+			pass
+
+		"Ask":
+			show("code")
+			pass
+
+		"Menu":
+			show("code")
+			pass
+
+		"Notify":
+			show("code")
+			pass
+
+		"Characters":
+			show("code")
+			pass
+			
+		"Using Global Variables":
+			show("code")
+			pass
+
+		"Go Back":
+			start("intro")
+
+	step()
+	hide("code")
+	start("script_tut")
+
+	end_event()
--- game/TutorialDialogue.gd
+++ game/TutorialDialogue.gd
@@ -5,11 +5,11 @@ func intro():
 	hide("code")
 	say(null, 
 		"This is Tutorial/Showcase Demo of Rakugo\n"
-		+ "Choose what you want to see/lean about."
+		+ "Choose what you want to see/learn about."
 	)
 	var begin_from = menu([
 		["Scripting Dialogue Tutorial", "scripting"],
-		["Markups"],
+		"Markups",
 		["Showing/Hide Character", "showing"],
 		["Jumping Between Scenes/Dialogues", "jumping"],
 		["More to Come", "more", {"disabled":true}]
@@ -17,7 +17,7 @@ func intro():
 
 	match begin_from:
 		"scripting":
-			start("script_tut")
+			$"../Scripting/Dialogue".start()
 
 		"Markups":
 			start("markups")
@@ -60,51 +60,6 @@ func markups():
 		"Go Back":
 			start("intro")
 
-
 	end_event()
 
-func script_tut():
-	start_event("script_tut")
 
-	say(null, "Choose Scripting Tutorial")
-	var script = menu([
-		"Creating Dialogue",
-		"Say", "Ask", "Menu",
-		"Notify", "Characters",
-		"Using Global Variables",
-		"Go Back"
-	])
-	
-	match script:
-		"Creating Dialogue":
-			show("code")
-			pass
-
-		"Say":
-			show("code")
-			pass
-
-		"Ask":
-			show("code")
-			pass
-
-		"Menu":
-			show("code")
-			pass
-
-		"Notify":
-			show("code")
-			pass
-
-		"Characters":
-			show("code")
-			pass
-			
-		"Using Global Variables":
-			show("code")
-			pass
-
-		"Go Back":
-			start("intro")
-
-	end_event()
