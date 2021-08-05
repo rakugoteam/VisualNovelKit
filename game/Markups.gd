e8a771c73686bca67cb56be57e33e14866bb65ef
Jeremi Biernacki <jeremi360@gmail.com>
2021-08-05 17:40:08 +0200

    add layout switcher

--- game/Markups.gd
+++ game/Markups.gd
@@ -1,13 +1,17 @@
 extends Dialogue
 
 export var intro_nodepath : NodePath
-export var options_nodepath : NodePath
+export var markups_options_nodepath : NodePath
+export var layout_options_nodepath : NodePath
+export var layout_nodepath : NodePath
 export var back_button_nodepath : NodePath
 export var tabs_nodepath : NodePath
 export var rakugo_text_label_nodepath : NodePath
 
 onready var parent : Control = get_parent() as Control
-onready var options : OptionButton = get_node(options_nodepath)
+onready var markups_options : OptionButton = get_node(markups_options_nodepath)
+onready var layout_options : OptionButton = get_node(layout_options_nodepath)
+onready var layout : GridContainer = get_node(layout_nodepath)
 onready var back_button : Button = get_node(back_button_nodepath)
 onready var tabs : TabContainer = get_node(tabs_nodepath)
 onready var rakugo_text_label : RakugoTextLabel = get_node(rakugo_text_label_nodepath)
@@ -17,9 +21,13 @@ var markup_id := 0
 var code_edit : CodeEdit
 
 func _ready():
-		options.connect("item_selected", self, "_on_option_selected")
-		switch_tab()
-		back_button.connect("pressed", self, "_on_back_button_pressed")
+	switch_tab()
+	markups_options.connect("item_selected", self, "_on_option_selected")
+	layout_options.connect("item_selected", self, "switch_layout")
+	back_button.connect("pressed", self, "_on_back_button_pressed")
+
+func switch_layout(id := 0):
+	layout.columns = id + 1
 
 func switch_tab(id:= 0):
 	var text := ""
@@ -43,13 +51,13 @@ func switch_tab(id:= 0):
 func _on_option_selected(id: int):
 	if id != markup_id:
 		switch_tab(id)
-		var markup = options.get_item_text(id)
+		var markup = markups_options.get_item_text(id)
 		rakugo_text_label.markup = markup.to_lower()
 
 	rakugo_text_label.rakugo_text = code_edit.text
 
 func _on_text_changed():
-	var id = options.get_selected_id()
+	var id = markups_options.get_selected_id()
 	_on_option_selected(id)
 
 func _on_back_button_pressed():	
