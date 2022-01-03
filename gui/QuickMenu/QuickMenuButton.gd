c5508eaa9131d540874cbb87d184fd534cf7823b
Jeremi Biernacki <jeremi360@gmail.com>
2022-01-03 19:20:46 +0100

    clean project

--- gui/QuickMenu/QuickMenuButton.gd
+++
@@ -1,8 +0,0 @@
-extends Button
-
-export var quick_action:String = ''
-
-signal quick_button_pressed(quick_action)
-
-func _on_pressed():
-	emit_signal("quick_button_pressed", quick_action)
--- gui/Screens/MenuBar/NavMenuButton.gd
+++
@@ -1,8 +0,0 @@
-extends Button
-
-export var nav_action:String = ""
-
-signal nav_button_pressed(nav_action)
-
-func _on_pressed():
-	emit_signal("nav_button_pressed", nav_action)
