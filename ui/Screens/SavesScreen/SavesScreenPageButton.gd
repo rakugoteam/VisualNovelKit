57b428e9657d64726c0a7cd5d0208eb767cd2541
Jeremi Biernacki <jeremi360@gmail.com>
2022-02-04 12:18:41 +0100

    this was bad idea to change this gui in this way, so I'm reverting it

--- ui/Screens/SavesScreen/SavesScreenPageButton.gd
+++
@@ -1,9 +0,0 @@
-extends Button
-
-signal page_button_pressed(action)
-
-func _ready():
-	connect("pressed", self, "_on_pressed")
-
-func _on_pressed():
-	emit_signal("page_button_pressed", self.text)
