85300abce7341de802af97a74c2301fc6603450b
Jeremi Biernacki <jeremi360@gmail.com>
2022-03-29 22:53:02 +0200

    remove ProjecTools

--- plug.gd
+++ plug.gd
@@ -3,7 +3,6 @@ extends "res://addons/gd-plug/plug.gd"
 func _plugging():
     # Declare your plugins in here with plug(src, args)
     # By default, only "addons/" directory will be installed
-    plug("rakugoteam/project-settings-helpers")
     plug("rakugoteam/Emojis-For-Godot", {"include": [".import/"]})
     plug("rakugoteam/Godot-Material-Icons", {"include": [".import/"]})
     plug("rakugoteam/AdvancedText")
