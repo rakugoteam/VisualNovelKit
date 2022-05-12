427d8d5fe442da37e89082445ec0544a9fae7042
Jeremi Biernacki <jeremi360@gmail.com>
2022-05-12 19:04:14 +0200

    update gd-plug config

--- plug.gd
+++ plug.gd
@@ -3,7 +3,10 @@ extends "res://addons/gd-plug/plug.gd"
 func _plugging():
     # Declare your plugins in here with plug(src, args)
     # By default, only "addons/" directory will be installed
-    plug("rakugoteam/Emojis-For-Godot", {"include": [".import/"]})
-    plug("rakugoteam/Godot-Material-Icons", {"include": [".import/"]})
+    plug("rakugoteam/Emojis-For-Godot", {"include": ["addons", ".import/"]})
+    plug("rakugoteam/Godot-Material-Icons", {"include": ["addons", ".import/"]})
     plug("rakugoteam/AdvancedText")
     plug("rakugoteam/Rakugo")
+    
+    ## comment-out if of you don't want to update kit itself
+    plug("rakugoteam/VisualNovelKit")
