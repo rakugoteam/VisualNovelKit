9d533405351df6ffb0abefc12566e5098096923b
Jeremi Biernacki <jeremi360@gmail.com>
2022-05-13 22:33:36 +0200

    fix bug on TextSpeedSlider that breaks game

--- addons/vnkit/Screens/PreferencesScreen/TextSpeedSlider.gd
+++ addons/vnkit/Screens/PreferencesScreen/TextSpeedSlider.gd
@@ -4,5 +4,5 @@ func _on_value_changed(value):
 	ProjectSettings.set_setting(VNKit.typing_effect_delay, abs(value))
 
 func _on_visibility_changed():
-	if visible and ProjectSettings.has_setting(VNKit.typing_effect_delay):
+	if visible:
 		value = -ProjectSettings.get_setting(VNKit.typing_effect_delay)
