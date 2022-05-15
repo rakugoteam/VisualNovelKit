25c9d7738b3c6637b4654c38729b93ec564b3a55
Jeremi Biernacki <jeremi360@gmail.com>
2022-05-15 17:47:52 +0200

    make hide works

--- addons/vnkit/InGameUI/QuickMenu/QuickMenu.gd
+++ addons/vnkit/InGameUI/QuickMenu/QuickMenu.gd
@@ -1,15 +1,37 @@
 extends Control
 
-
-signal quick_button_press(quick_action)
-
-func _ready():
-	connect_buttons()
-
-func connect_buttons():
-	for qb in get_tree().get_nodes_in_group("quick_button"):
-		qb.connect("quick_button_pressed", self, "_on_quick_button_pressed")
+func _on_Hide_pressed():
+	Window.Tabs.get_child(1).hide()
 
 
-func _on_quick_button_pressed(quick_action):
-	emit_signal("quick_button_press", quick_action)
+func _on_History_pressed():
+	pass # Replace with function body.
+
+
+func _on_Back_pressed():
+	pass # Replace with function body.
+
+
+func _on_Skip_pressed():
+	pass # Replace with function body.
+
+
+func _on_Save_pressed():
+	pass # Replace with function body.
+
+
+func _on_Load_pressed():
+	pass # Replace with function body.
+
+
+func _on_Preferences_pressed():
+	pass # Replace with function body.
+
+
+func _on_Quit_pressed():
+	pass # Replace with function body.
+
+func _process(delta):
+	if Input.is_action_just_pressed("hide_ui"):
+		var ui_visible = Window.Tabs.get_child(1).visible
+		Window.Tabs.get_child(1).visible = not ui_visible
