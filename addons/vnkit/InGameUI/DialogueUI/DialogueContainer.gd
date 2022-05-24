d1d47d506e81c054ee7aba06ee28934f357c97ed
Jeremi Biernacki <jeremi360@gmail.com>
2022-05-24 23:02:49 +0200

    update DialogueUI to character being now a Dictionary

--- addons/vnkit/InGameUI/DialogueUI/DialogueContainer.gd
+++ addons/vnkit/InGameUI/DialogueUI/DialogueContainer.gd
@@ -3,20 +3,19 @@ extends VBoxContainer
 onready var dialog_label := $DialogLabel
 onready var answer_edit := $AnswerEdit
 
-func _ready():
+func _ready() -> void:
 	Rakugo.connect("say", self, "_on_say")
 	Rakugo.connect("ask", self, "_on_ask")
 	Rakugo.connect("step", self, "_on_step")
 	answer_edit.connect("text_entered", self, "_on_answer_entered")
 
-func _on_say(character, text):
+func _on_say(character:Dictionary, text:String) -> void:
 	show()
-	# TODO: make line below work
-	# $DialogLabel.variables = Rakugo.variables
-	if character == null:
+	if character.empty():
 		character = Rakugo.get_narrator()
-
-	text = "# " +  character.name + "\n" + text
+	
+	var ch_name = character.get("name", "null")
+	text = "# %s \n%s" % [ch_name, text]
 	dialog_label.markup_text = text
 	# prints("dialog_label:", dialog_label.bbcode_text)
 
@@ -24,19 +23,19 @@ func _on_step():
 	dialog_label.markup_text += "\n@shake 5, 10 {Press 'Enter' to continue...}"
 	# hide()
 
-func _on_ask(character:Character, question:String, default_answer:String):
+func _on_ask(character:Dictionary, question:String, default_answer:String) -> void:
 	_on_say(character, question)
 	answer_edit.show()
 	answer_edit.grab_focus()
 	answer_edit.placeholder_text = default_answer
 
-func _on_ask_entered(answer):
+func _on_ask_entered(answer:String) -> void:
 	answer_edit.hide()
 	answer_edit.placeholder_text = ""
 	answer_edit.text = ""
 	Rakugo.ask_return(answer)
 
-func _process(delta):
+func _process(delta) -> void:
 	var ui_accept := Input.is_action_just_pressed("ui_accept")
 	if Rakugo.is_waiting_step() and ui_accept:
 		Rakugo.do_step()
