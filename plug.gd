a60801611fb3096cda301351a4233065cf60fe7d
Jeremi Biernacki <jeremi360@gmail.com>
2022-09-07 08:56:40 +0200

    remove BaseKit - I want use it any more

--- plug.gd
+++ plug.gd
@@ -1,19 +1,12 @@
 extends "res://addons/gd-plug/plug.gd"
 
+
 func _plugging():
 	# Declare your plugins in here with plug(src, args)
 	# By default, only "addons/" directory will be installed
 	plug("rakugoteam/Emojis-For-Godot", {"include": ["addons", ".import/"]})
 	plug("rakugoteam/Godot-Material-Icons", {"include": ["addons", ".import/"]})
 	plug("rakugoteam/Rakugo", {"include": ["addons/rakugo", ".import/"]})
-	plug("rakugoteam/BaseKit", {
-		"include": ["addons/kit", "addons/gd-plug"],
-		"exclude": ["addons/kit/plugin.cfg"],
-		"on_update": "post_kit_update"
-	})
-	
+
 	## comment-out if of you don't want to update kit itself
 	# plug("rakugoteam/VisualNovelKit", {"include": ["addons", "plug.gd"]})
-
-func post_kit_update(plugin : Dictionary):
-	directory_copy_recursively("addons/kit/", "addons/vnkit")
